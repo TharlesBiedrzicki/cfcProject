4d3dbd1586aec4d517a53a19265a0ce3b6383d5e
TharlesBiedrzicki <tbiedrzicki@gmail.com>
2019-10-15 21:44:30 -0300

    classes do projeto

---
+++ src/projetocfc/ProjetoCFC.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetocfc;
+
+/**
+ *
+ * @author Tharles
+ */
+public class ProjetoCFC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/projetocfc/pessoa.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetocfc;
+
+/**
+ *
+ * @author Tharles
+ */
+public class pessoa {
+    private String nome;
+    private String nascimento;
+    private String RG;
+    
+}
